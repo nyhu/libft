b6238d99cfd3a9bcb02757a56aa1917895b78b4b
Toussaint BOOS <tboos@e2r4p11.42.fr>
2016-03-16 14:16:54 +0100

    add null sur retour memcpy

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: tboos <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/04 18:24:51 by tboos             #+#    #+#             */
-/*   Updated: 2016/02/22 20:42:32 by tboos            ###   ########.fr       */
+/*   Updated: 2016/03/16 14:16:07 by tboos            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,5 +24,7 @@ void				*ft_memcpy(void *dest, const void *src, size_t n)
 			src++;
 			to++;
 		}
+	else
+		return (NULL);
 	return (dest);
 }
