a9de574747ec6e82f75926cf68bcdf2106a8e4ae
nyhu <toussaint.boos@gmail.com>
2016-03-25 06:12:27 +0100

    enlevage recursive

--- ft_putstrtab.c
+++ ft_putstrtab.c
@@ -2,22 +2,26 @@
 
 void	ft_putstrtab_fd(char const **s, char c, int fd)
 {
-	if (*s)
+	int	i;
+
+	i = -1;
+	while (s[++i])
 	{
-		ft_putstr_fd(*s, fd);
-		if (*(s + sizeof(char *)))
+		ft_putstr_fd(s[i], fd);
+		if (s[i + 1])
 			ft_putchar_fd(c, fd);
-		ft_putstrtab_fd((char const **)(s + sizeof(char *)), c, fd);
 	}
 }
 
 void	ft_putstrtab(char const **s, char c)
 {
-	if (*s)
+	int	i;
+
+	i = -1;
+	while (s[++i])
 	{
-		ft_putstr(*s);
-		if (*(s + sizeof(char *)))
+		ft_putstr(s[i]);
+		if (s[1 + 1])
 			ft_putchar(c);
-		ft_putstrtab((char const **)(s + sizeof(char *)), c);
 	}
 }
