00c9171b8249a0b964f021ebb86975a8679de9ff
nyhu <toussaint.boos@gmail.com>
2016-03-26 01:33:15 +0100

    ajout strchrjoin

--- basics/ft_strslashjoin.c
+++ basics/ft_strslashjoin.c
@@ -12,6 +12,19 @@
 
 #include "libft.h"
 
+char	*ft_strchrjoin(char *s1, char c, char *s2)
+{
+	char	*new;
+
+	if (!s1 || !s2 || !(*s1) || !(*s2))
+		return (NULL);
+	new = (char *)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 2);
+	new = ft_strcpy(new, s1);
+	new[strlen(new)] = c;
+	new = ft_strcat(new, s2);
+	return (new);
+}
+
 char	*ft_strslashjoin(char *s1, char *s2)
 {
 	char	*new;
--- includes/basics.h
+++ includes/basics.h
@@ -36,6 +36,7 @@ char	*ft_strmapi(char const *s, char(*f)(unsigned int, char));
 ** STRING WORK
 */
 char	*ft_strslashjoin(char *s1, char *s2);
+char	*ft_strchrjoin(char *s1, char c, char *s2);
 char	*ft_strtrim(char const *s);
 char	**ft_strsplit(char *s, char c);
 char	**ft_strncmptabdel(char **dest, char *needle);
